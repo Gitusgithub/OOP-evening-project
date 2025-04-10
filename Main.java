f3e050ac1468305be65724744cb98c611359d7e7
Gitusgithub <gitujohn9@gmail.com>
2025-04-10 21:45:16 +0300

    commit 1 and renaming

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    
+}
+import java.util.Scanner;
+import java.time.LocalDate;
+
+class Student {
+    String name;
+    String id;
+    int[] marks = new int[5];
+    double average;
+    String grade;
+    String comment;    
